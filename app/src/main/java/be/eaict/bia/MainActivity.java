0a9cdf236808031ae6f5d6f11e7043514224e939
Wim Geentjens <wimgeentjens@msn.com>
2017-12-13 11:00:53 +0100

    push test

--- app/src/main/java/be/eaict/bia/MainActivity.java
+++ app/src/main/java/be/eaict/bia/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         Bier b = new Bier("Stella",1.60);
         c.AddBier(b);
 
+        //blablabla
+
         myRef.push().setValue(c);
     }
 }
